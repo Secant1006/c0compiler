b209306e0b27344dd408da21a5b6429a62429085
Secant1006 <secant1006@outlook.com>
2019-12-27 01:25:24 +0800

    Add compiling script

---
+++ make.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir out
+javac -cp ./src -d ./out ./src/com/secant/c0compiler/console/Main.java
+jar cfm cc0.jar ./meta-inf/manifect.mf -C ./out .
+rm -rf ./out
+echo '#!/bin/bash'>./cc0
+echo 'java -jar cc0.jar $1 $2 $3 $4'>>./cc0
+chmod +x ./cc0
